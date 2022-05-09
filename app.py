eaceab6b9e6cd7ff32dc975fe7595f86f58d1ef8
Pacific44 <100478000+Pacific44@users.noreply.github.com>
2022-05-09 13:22:46 +0530

    Update app.py
    add mongo client

--- app.py
+++ app.py
@@ -12,13 +12,14 @@ import sys
 import subprocess
 import shlex
 
-#myclient = MongoClient("mongodb+srv://admin:SMUQis3ismx7zN4b@cluster44.zotew.mongodb.#net/myFirstDatabase?retryWrites=true&w=majority")
+myclient = MongoClient("mongodb://root:N5tfgb9QExIGYXIq@cluster0-shard-00-00.e4swe.mongodb.net:27017,cluster0-shard-00-01.e4swe.mongodb.net:27017,cluster0-shard-00-02.e4swe.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-biz1ll-shard-0&authSource=admin&retryWrites=true&w=majority")
 def get_db():
-    client = MongoClient(host='20.219.136.154',
-                         port=27017,
-                         username='Customer',
-                         password='Customer',
-                         authSource="admin")
+    #client = MongoClient(host='20.219.136.154',
+     #                    port=27017,
+      #                   username='Customer',
+       #                  password='Customer',
+        #                 authSource="admin")
+    client = myclient
     db = client["Customer"]
     return db
 
